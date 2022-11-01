3237bbf4cd68e38fb7ad0d6f05c9aaff5004d873
cristhian <cristhianjhl.web@gmail.com>
2022-11-01 10:19:42 -0500

    add: new validations of transactions

--- src/v1/controllers/TransactionController.js
+++ src/v1/controllers/TransactionController.js
@@ -44,7 +44,37 @@ class TransactionController {
     }
 
     async transfer(req, res) {
-        const { fromId, toId, amount } = req.params;
+        const { fromId, toId } = req.params;
+        const { amount } = req.body;
+
+        const fromEnvelope = await DB.select("SELECT * FROM envelopes WHERE id = $1", [fromId]);
+
+        if (!fromEnvelope || fromEnvelope.code === 400) return res.status(404).json({
+            status: "FAIL",
+            code: 404,
+            error: `Envelope with id ${fromId} not found.`,
+        });
+
+        const toEnvelope = await DB.select("SELECT * FROM envelopes WHERE id = $1", [toId]);
+        
+        if (!toEnvelope || toEnvelope.code === 400) return res.status(404).json({
+            status: "FAIL",
+            code: 404,
+            error: `Envelope with id ${toId} not found.`,
+        });
+
+        if (!amount) return res.status(404).json({
+            status: "FAIL",
+            code: 404,
+            error: "The amount param is required",
+        });
+
+        if (fromEnvelope.data[0].budget < amount) return res.status(404).json({
+            status: "FAIL",
+            code: 404,
+            error: `Not enought budget on envelope '${fromEnvelope.data[0].title}'`,
+        });
+            
         const result = await DB.insert("INSERT INTO transactions (from_id, to_id, amount) VALUES ($1, $2, $3)", [fromId, toId, amount]);
         await DB.update("UPDATE envelopes SET budget = (budget - $1) WHERE id = $2", [amount, fromId]);
         await DB.update("UPDATE envelopes SET budget = (budget + $1) WHERE id = $2", [amount, toId]);
